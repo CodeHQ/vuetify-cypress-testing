a6b418a36f93ad4184734fcbbcc71f959f506282
build-server <build-server@codehq.co.za>
2021-02-01 13:14:35 +0200

    Added responsiveScreenShots

--- commands/index.js
+++ commands/index.js
@@ -18,3 +18,30 @@ Cypress.Commands.add('vTimerSelect', (selector, hourNumber, minuteNumber, pm) =>
   }
   cy.get('[data-cy=TimeField-Ok]').click();
 });
+Cypress.Commands.add('responsiveScreenShots', (name) => {
+  const viewPorts = [
+    'ipad-2',
+    'ipad-mini',
+    'iphone-3',
+    'iphone-4',
+    'iphone-5',
+    'iphone-6',
+    'iphone-6+',
+    'iphone-7',
+    'iphone-8',
+    'iphone-x',
+    'iphone-xr',
+    'iphone-se2',
+    'macbook-11',
+    'macbook-13',
+    'macbook-15',
+    'macbook-16',
+    'samsung-note9',
+    'samsung-s10',
+  ];
+  viewPorts.forEach((viewPort) => {
+    cy.viewport(viewPort);
+    cy.screenshot(`${name}-${viewPort}`);
+  });
+  cy.viewport(1000, 600);
+});
